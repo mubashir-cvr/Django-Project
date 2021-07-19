93db8dc7dec924128ad11b69905978f1e02642a8
Mubashir V <mubashircheruvayoor@gmail.com>
2021-07-20 00:17:33 +0530

    Create Super user

--- home/apps.py
+++ home/apps.py
@@ -1,6 +1,14 @@
 from django.apps import AppConfig
+from django.contrib.auth import get_user_model
 
 
 class HomeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'home'
+
+
+def ready():
+    User = get_user_model()
+    if User.objects.exists():
+        return
+    User.objects.create_superuser(username='admin', password='admin@123', email='admin@admin.com')
