483a43fa96af6612af39d037e1629da5345e5ce3
FathimaShirin <shirinkfathima@gmail.com>
2021-07-19 21:46:52 +0530

    Add Statics

--- servicer/urls.py
+++ servicer/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('booking/', include('serviceBooking.urls')),
     path('admin/', admin.site.urls),
 ]
-urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  # register media path
+urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)  # register media path
